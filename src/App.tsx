946f519ca346d93f4b51b50c31c26033e2ddd8d7
Thais Gomes de oliveira e Silva <t.silva@viainvest.local>
2025-07-22 08:47:26 -0300

    add DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,47 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 
 export function App() {
 
     return (
         <>
-    <Container>
-        <Logo></Logo>
-    </Container>
-    <Container>
-        <Menu></Menu>
-    </Container>
-    <Container>
-    <CountDown></CountDown>
-    </Container>
-    <Container>
-        <section>FOOTER</section>
-    </Container>
-           
+            <Container>
+                <Logo></Logo>
+            </Container>
+            <Container>
+                <Menu></Menu>
+            </Container>
+            <Container>
+                <CountDown></CountDown>
+            </Container>
+            <Container>
+                <form action="" className="form">
+                    <div className="formRow">
+                        <DefaultInput 
+                          type='text' 
+                          id='meuInput' 
+                          labelText='Task'          
+                          placeholder="Insira a sua tarefa "
+                          />
+                    </div>
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className="formRow">
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0 </p>
+                    </div>
+
+                    <div className="formRow">
+                        <button>Enviar</button>
+                    </div>
+                </form>
+
+            </Container>
         </>
     );
 }
